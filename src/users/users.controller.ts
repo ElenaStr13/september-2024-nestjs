8d29567c3d05173168bf2d1111f9f834d6b02e33
ElenaStr13 <115697145>
2024-10-16 11:31:07 +0300

    lesson 2

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,16 +7,20 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiConflictResponse, ApiTags } from '@nestjs/swagger';
 
 import { CreateUserReqDto } from './dto/req/create-user.req.dto';
 import { UpdateUserReqDto } from './dto/req/update-user.req.dto';
 import { UserResDto } from './dto/res/user.res.dto';
 import { UsersService } from './users.service';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiBearerAuth()
+  @ApiConflictResponse({ description: 'Conflict' })
   @Post()
   create(@Body() createUserDto: CreateUserReqDto): Promise<UserResDto> {
     return this.usersService.create(createUserDto);
