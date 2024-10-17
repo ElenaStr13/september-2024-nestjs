c29d21b4cff408bde5d8e63064ac9b5d27ac48ad
ElenaStr13 <115697145>
2024-10-17 14:34:01 +0300

    lesson 4

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,10 @@
 import { forwardRef, Module } from '@nestjs/common';
 
 import { ArticlesModule } from '../articles/articles.module';
+import { UsersService } from './services/users.service';
+import { UsersAdminService } from './services/users-admin.service';
 import { UsersAdminController } from './user.admin.controller';
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
-import { UsersAdminService } from './users-admin.service';
 
 @Module({
   imports: [forwardRef(() => ArticlesModule)],
